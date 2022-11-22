3572796947d9b03131b08414480860d79056d476
arthurKap <marlboro2224@gmail.com>
2022-11-22 18:11:55 +0200

    add form

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,29 +2,15 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
-  state = {
-    done: false,
-    important: false,
-  };
-  onLabelClick = () => {
-    this.setState(({ done }) => {
-      return {
-        done: !done,
-      };
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState(({ important }) => {
-      return {
-        important: !important,
-      };
-    });
-  };
-
   render() {
-    const { label, onDeleted } = this.props;
-    const { done, important } = this.state;
+    const {
+      label,
+      onDeleted,
+      onToggeleImportant,
+      onToggeleDone,
+      important,
+      done,
+    } = this.props;
 
     let classNames = "todo-list-item";
     if (done) {
@@ -37,14 +23,14 @@ export default class TodoListItem extends Component {
 
     return (
       <span className={classNames}>
-        <span className="todo-list-item-label" onClick={this.onLabelClick}>
+        <span className="todo-list-item-label" onClick={onToggeleDone}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-end"
-          onClick={this.onMarkImportant}
+          onClick={onToggeleImportant}
         >
           <i className="bi bi-exclamation" />
         </button>
